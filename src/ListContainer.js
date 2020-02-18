31b0caeec526e39fe4a2ec6543151238385fb7d3
malsaadan <mmalsaadan@gmail.com>
2020-02-18 14:30:59 +0300

    fruit-app init

---
+++ src/ListContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+const fruits = ['Mango', 'Banana', 'Orange', 'Apple', 'Pineapple', 'Dates'];
+
+export default class ListContainer extends React.Component {
+    render(){
+        const allFruits = fruits.map(function(fruit, index){
+            return <ListItem name={fruit} key={index} />;
+        })
+
+        return(
+            <ul>
+                {/* <ListItem name={'name'}/>
+                <ListItem name={'name'}/>
+                <ListItem name={'name'}/>
+                <ListItem name={'name'}/> */}
+                
+                {allFruits}
+            </ul>
+        );
+    }
+}
